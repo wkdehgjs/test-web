56ed54400f548899c2599150a28c61961491ee66
wkdehgjs <pirisori0705@gmail.com>
2024-06-08 13:30:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.getElementById('blackScreen').addEventListener('click', function(event) {
+    // 이미지 생성
     const img = document.createElement('img');
-    img.src = 'path/to/your/image.jpg';
+    img.src = 'images/myImage.jpg'; // JPG 이미지 파일 경로를 여기에 입력하세요
     img.style.position = 'absolute';
     img.style.left = `${event.clientX}px`;
     img.style.top = `${event.clientY}px`;
@@ -8,6 +9,7 @@ document.getElementById('blackScreen').addEventListener('click', function(event)
     
     document.getElementById('blackScreen').appendChild(img);
     
+    // 이미지 1초 후 제거
     setTimeout(() => {
         img.remove();
     }, 1000); // 1초 후에 이미지가 사라짐
